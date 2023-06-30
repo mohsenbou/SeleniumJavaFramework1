6f9edfd6dc0183859c640f0c75f1a419971cbf0a
Mohsen Bouchkara <mohsenbou@gmail.com>
2023-06-30 14:24:17 +0200

    Selenium TestNG Ignore
    package-info.java: prevent the whole tests execution in the package "demo". we need to disable the ignore notation.

---
+++ src/test/java/demo/TestNGIgnoreDemo.java
@@ -0,0 +1,21 @@
+package demo;
+
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+//@Ignore
+public class TestNGIgnoreDemo {
+
+    @Test
+    @Ignore
+    public void test1(){
+        System.out.println("I am inside test 1");
+    }
+    @Test
+    public void test2(){
+        System.out.println("I am inside test 2");
+    }
+    @Test
+    public void test3(){
+        System.out.println("I am inside test 3");
+    }
+}
